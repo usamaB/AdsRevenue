f50a553fd539defb1f4cb8ea075a10d1fa4408c4
usamaB <usama.kaleem88@gmail.com>
2020-07-26 18:37:06 +0200

    separate parameter parsing

---
+++ src/main/scala/org/usama/adsrevenue/utils/ParameterParser.scala
@@ -0,0 +1,57 @@
+package org.usama.adsrevenue.utils
+
+import com.monovore.decline.{Command, Opts}
+import cats.implicits._
+
+case class AppParameters(
+    impressionsPath: String,
+    clicksPath: String,
+    outputPath: String
+)
+
+/**
+  * Using decline a command-line parser built on cats
+  * https://github.com/bkirwi/decline
+  * Creating a command and the arguments it needs
+  */
+object ParameterParser {
+
+  private def getOpts() = {
+    val impressionsFilePath =
+      Opts
+        .option[String]("impressions-file-path", "absolute path for file", "i")
+    val clicksFilePath =
+      Opts.option[String]("clicks-file-path", "absolute path for file", "c")
+    val outputFilePath =
+      Opts
+        .option[String]("output-file-path", "absolute path for file", "o")
+        .withDefault("metrics.json")
+
+    (impressionsFilePath, clicksFilePath, outputFilePath).tupled
+  }
+
+  private def getCommand = {
+    Command(name = "metrics", header = "calculate ad metrics") {
+      getOpts()
+    }
+  }
+
+  /**
+    * Parses the applications parameters
+    * In case of Failure prints error and exits the system else return AppParameters
+    */
+  def parse(args: Array[String]): AppParameters = {
+    getCommand.parse(args.toIndexedSeq) match {
+      case Left(help) =>
+        System.err.println(help)
+        sys.exit(1)
+      case Right(parsedValue) =>
+        AppParameters(
+          parsedValue._1,
+          parsedValue._2,
+          parsedValue._3
+        )
+    }
+  }
+
+}
